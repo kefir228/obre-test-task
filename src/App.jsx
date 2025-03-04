92b11b6e9425260120910535518385eeb164b565
kefir228 <oleg05karp@gmail.com>
2025-03-04 22:17:19 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-10 text-2xl text-center m-0">
+      uebfivrivi
+    </div>
+  )
+}
+
+export default App
