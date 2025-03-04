9670934e03f3db6622a6d3232a9106bd3e4d29ea
kefir228 <oleg05karp@gmail.com>
2025-03-04 22:21:37 +0200

    create demo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/obre-test-task/',
   plugins: [react()],
   css: {
     devSourcemap: true,
