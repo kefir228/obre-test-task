3b4823140c7f094fd2420b67520d944c5468e1dc
kefir228 <oleg05karp@gmail.com>
2025-03-05 13:25:32 +0200

    debug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,18 +9,21 @@ export const Header = ({ openModal, isChecked, setIsChecked, setSearchQuery }) =
     }
 
     return (
-        <div className="flex justify-around items-center gap-4 mb-4">
-            <label>
+        <div className="flex flex-col md:flex-row justify-between items-center gap-4 mb-4 px-4">
+            <label className="flex items-center gap-2">
                 <input type="checkbox" onChange={handleCheckBox} checked={isChecked} />
-                Сортувати за алфавітом
+                <span className="text-sm md:text-base">Сортувати за алфавітом</span>
             </label>
             <input
                 type="text"
                 placeholder="Шукати за ім'ям"
                 onChange={handleSearch}
-                className="border p-2 rounded-lg"
+                className="flex-1 border p-2 rounded-lg text-sm md:text-base"
             />
-            <button onClick={openModal} className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg">
+            <button
+                onClick={openModal}
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg text-sm md:text-base w-full md:w-auto"
+            >
                 Додати нового
             </button>
         </div>
